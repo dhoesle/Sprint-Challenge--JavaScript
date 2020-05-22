b2c3e896cc960bf4ed65d67c6e1f0f2b07a76c10
Daniel Hoesle <d1@PacBook-Mro.local>
2020-05-22 10:44:59 -0600

    completed request 3 on arrays-callbacks

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -46,7 +46,9 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(function(item){
+  return item['population'] < 5;
+})
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -66,19 +68,32 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, cb) {
+  return cb(a,b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+};
 
+function multiply(num1, num2) {
+  let product = num1 * num2;
+  return product;
+};
 
+function greeting(fName, lName) {
+  return `Hello ${fName} ${lName}, nice to meet you!`;
+};
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
